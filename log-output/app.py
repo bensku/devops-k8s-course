2c2cccc7f6150fdf69f9676138b2a2ef5ef23fd5
bensku <bmi@iki.fi>
2024-09-14 16:02:13 +0300

    1.07: log-output is a webapp now, I guess

--- log-output/app.py
+++ log-output/app.py
@@ -1,9 +1,22 @@
 import datetime
+import threading
 import time
 import uuid
+from fastapi import FastAPI
+
+app = FastAPI()
 
 random_str = str(uuid.uuid4())
+last_time = datetime.datetime.now()
 
-while True:
-    print(datetime.datetime.now(), random_str, flush=True)
-    time.sleep(5)
+def update_status():
+    while True:
+        last_time = datetime.datetime.now()
+        print(last_time, random_str, flush=True)
+        time.sleep(5)
+
+threading.Thread(target=update_status).start()
+
+@app.get("/status")
+def read_root():
+    return {'time': last_time, 'status': random_str}
