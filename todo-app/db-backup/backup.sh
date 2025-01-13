56233257d11d6dde41b5cb29ebaa5dcd3bbafda3
Benjami Luolajan-Mikkola <benjami.luolajan-mikkola@solita.fi>
2025-01-13 17:04:49 +0200

    Tweak the backup script a bit

--- todo-app/db-backup/backup.sh
+++ todo-app/db-backup/backup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-pg_dump -v postgresql://postgres:$DB_PASSWORD@$DB_HOST/postgres >/tmp/backup.sql
+DB_PASSWORD=$(echo "$DB_PASSWORD" | tr -d '\n')
+pg_dump -v "postgresql://postgres:$DB_PASSWORD@$DB_HOST/postgres" >/tmp/backup.sql
 
 datetime=$(date +%Y%m%d%H%M%S)
 gcloud storage cp /tmp/backup.sql gs://$BACKUP_TARGET/backup-$datetime.sql
