892df36faa92c56c6aecddacdbca884ca8015bc7
Benjami Luolajan-Mikkola <benjami.luolajan-mikkola@solita.fi>
2025-01-13 16:18:48 +0200

    Exercise 3.07: Take a stab at backups!

---
+++ todo-app/db-backup/backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -euo pipefail
+
+pg_dump -v postgresql://postgres:$DB_PASSWORD@$DB_HOST/postgres >/tmp/backup.sql
+
+datetime=$(date +%Y%m%d%H%M%S)
+gcloud storage cp /tmp/backup.sql gs://$BACKUP_TARGET/backup-$datetime.sql
